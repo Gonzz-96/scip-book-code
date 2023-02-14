541514cbe8df8f0b69b3ade79725471b8142eba8
Gonzalo Campos <gonzalocampose1@gmail.com>
2023-02-13 22:48:02 -0600

    Code.

--- src/sicp_code/ch1/code.clj
+++ src/sicp_code/ch1/code.clj
@@ -66,4 +66,42 @@
 ;; (sqrt (+ 100 37)
 ;; (square (sqrt 1000)
 
+;; 1.2.2 tree recursion
+(defn fib [n]
+  (cond (= n 0) 0
+        (= n 1) 1
+        :else (+ (fib (- n 1))
+                 (fib (- n 2))))) ;; => not tail-recursive
+
+(defn iterative-fib [n]
+  (defn fib-iter
+    [a b count]
+    (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n))
+
+;; Example: counting change
+
+(defn count-change [amount]
+  (cc amount 5))
+
+(defn cc
+  [amount kinds-of-coins]
+  (cond (= amount 0) 1
+        (or (< amount 0) (= kinds-of-coins 0)) 0
+        :else (+ (cc amount (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins))))
+
+(defn first-denomination
+  [kinds-of-coins]
+  (cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 5
+        (= kinds-of-coins 3) 10
+        (= kinds-of-coins 4) 25
+        (= kinds-of-coins 5) 50))
+
+;; (count-change 100)
 
